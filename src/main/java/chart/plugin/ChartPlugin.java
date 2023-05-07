90b87424a599b2822ee0bd953dc8d163c8d0a31a
Alisha Listya <alisha.listya@gmail.com>
2023-05-07 21:24:07 +0700

    add: initial commit

--- src/main/java/chart/plugin/ChartPlugin.java
+++ src/main/java/chart/plugin/ChartPlugin.java
@@ -1,3 +1,5 @@
+package chart.plugin;
+
 import chart.plugin.ChartPluginInterface;
 import controller.MainController;
 import controller.fixedbill.FixedBillAdapterXML;
@@ -17,19 +19,4 @@ public class ChartPlugin implements ChartPluginInterface {
         List<FixedBill> temp = mainController.getFixedBillDataIO().getAll();
         return new XChartPanel<>(new PieChartPlugin(temp).getChart());
     }
-//    private static void createAndShowGUI() {
-//        JFrame frame = new JFrame("XChart Demo");
-//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//        MainController m = new MainController();
-//        m.setFixedBillDataIO(new FixedBillAdapterXML("src/main/resources/data/tes_fixed_bill.xml"));
-//        frame.add(loadPanel(m));
-//
-//        frame.pack();
-//        frame.setVisible(true);
-//    }
-
-//    public static void main(String[] args) {
-//        javax.swing.SwingUtilities.invokeLater(
-//                ChartPlugin::createAndShowGUI);
-//    }
 }
